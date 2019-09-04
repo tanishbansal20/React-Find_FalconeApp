d86762489908f811814ab0a6641e0d3f07555182
Tanish Bansal <tanish@SeanergyMacPro007s-MacBook-Pro.local>
2019-09-04 13:55:58 +0530

    working code

--- src/components/destination/Destination.js
+++ src/components/destination/Destination.js
@@ -32,6 +32,7 @@ export default class Destination extends Component {
 			destination2Vehicals: [],
 			destination3Vehicals: [],
 			destination4Vehicals: [],
+			vehicalsSpeed: {},
 		}
 	}
 
@@ -51,6 +52,12 @@ export default class Destination extends Component {
 			})
 	}
 
+	setTotalVehicals(data) {
+		const vehicalsSpeed = {};
+		data.forEach((obj) => { vehicalsSpeed[obj.name] = obj.speed });
+		return vehicalsSpeed;
+	}
+
 	componentDidMount() {
 		axios.get('https://findfalcone.herokuapp.com/planets')
 			.then((response) => {
@@ -66,6 +73,7 @@ export default class Destination extends Component {
 			.then((response) => {
 				this.setState ({
 					vehicals: response.data,
+					vehicalsSpeed: this.setTotalVehicals(response.data),
 				});
 			})
 			.catch((error) => {
@@ -74,17 +82,194 @@ export default class Destination extends Component {
 	}
 
 	getSelectedPlanetDistance(selectedPlanet) {
-		return this.state.planets.filter(planet => { if(planet.name === selectedPlanet){ return planet}})[0].distance;
+		if(selectedPlanet.length) {
+			return this.state.planets.filter(planet => { if(planet.name === selectedPlanet){ return planet}})[0].distance;
+		}
+		return 0;
 	}
 
-	getVehicalsObject(destinationVehicals, selectedPlanet, selectedVehical) {
-		return destinationVehicals.map(vehical => {
-			if(vehical.max_distance < this.getSelectedPlanetDistance(selectedPlanet) || (vehical.total_no === 0 && vehical.name !== selectedVehical)) {
-				return { value:vehical.name, label:`${vehical.name} (${vehical.total_no})`, itemClassName:"disabled" }
-			} else {
-				return { value:vehical.name, label:`${vehical.name} (${vehical.total_no})`}
-			}
+	getSpeed(selectedVehical) {
+		if(selectedVehical.length) {
+			return this.state.vehicals.filter(vehical => { if(vehical.name === selectedVehical) { return vehical}})[0].speed
+		}
+		return 0;
+	}
+
+	getCount() {
+		let count = 0;
+		const {selectedVehical1, selectedVehical2, selectedVehical3, selectedVehical4} = this.state;
+		if(selectedVehical1) {
+			count += this.state.vehicalsSpeed[selectedVehical1];
+		}
+		if(selectedVehical2) {
+			count += this.state.vehicalsSpeed[selectedVehical2];
+		}
+		if(selectedVehical3) {
+			count += this.state.vehicalsSpeed[selectedVehical3];
+		}
+		if(selectedVehical4) {
+			count += this.state.vehicalsSpeed[selectedVehical4];
+		}
+		return count;
+	}
+
+	increaseVehicalNumber(vehicals, vehicalName) {
+		const {selectedVehical1, selectedVehical2, selectedVehical3, selectedVehical4} = this.state;
+		 return vehicals.map(vehical => {
+			 if(vehical.name === vehicalName) {
+				 vehical.total_no +=1;
+			 }
+			 if(vehical.name === selectedVehical1) {
+				 vehical.total_no -=1;
+			 }
+			 if(vehical.name === selectedVehical2) {
+				 vehical.total_no -=1;
+			 }
+			 if(vehical.name === selectedVehical3) {
+				 vehical.total_no -=1;
+			 }
+			 if(vehical.name === selectedVehical4) {
+				 vehical.total_no -=1;
+			 }
+
+			 return {...vehical}
+		 });
+	}
+
+	resetDestinationVehicals(destinationVehicals, selectedVehical){
+		this.setState ({
+			[destinationVehicals]: this.state.vehicals.map(vehical => {return {...vehical}}),
+			[selectedVehical]: '',
 		});
+	}
+
+	decreaseVehicalNumber(vehicals, vehicalName) {
+		const {selectedVehical1, selectedVehical2, selectedVehical3, selectedVehical4} = this.state;
+		return vehicals.map(vehicalObj => {
+			if(vehicalObj.name === vehicalName) {
+				vehicalObj.total_no -=1;
+			}
+			if(vehicalObj.name === selectedVehical1){
+				vehicalObj.total_no -=1;
+				if(vehicalObj.total_no === -1){
+					vehicalObj.total_no = 0;
+					this.resetDestinationVehicals('destination1Vehicals', 'selectedVehical1');
+				}
+			}
+			if(vehicalObj.name === selectedVehical2){
+				vehicalObj.total_no -=1;
+				if(vehicalObj.total_no === -1){
+					vehicalObj.total_no = 0;
+					this.resetDestinationVehicals('destination2Vehicals', 'selectedVehical2');
+				}
+			}
+			if(vehicalObj.name === selectedVehical3){
+				vehicalObj.total_no -=1;
+				if(vehicalObj.total_no === -1){
+					vehicalObj.total_no = 0;
+					this.resetDestinationVehicals('destination3Vehicals', 'selectedVehical3');
+				}
+			}
+			if(vehicalObj.name === selectedVehical4){
+				vehicalObj.total_no -=1;
+				if(vehicalObj.total_no === -1){
+					vehicalObj.total_no = 0;
+					this.resetDestinationVehicals('destination4Vehicals', 'selectedVehical4');
+				}
+			}
+			return vehicalObj
+		});
+	}
+
+
+
+	async vehicalSelect (event, destination) {
+		switch (destination) {
+			case 'Destination1':
+				if(this.state.selectedVehical1.length) {
+					await this.setState ({
+						destination1Vehicals: this.state.vehicals.map(vehical => { return {...vehical}}),
+						selectedVehical1: '',
+					});
+				}
+				this.setState ({
+					selectedVehical1: event,
+					destination1Vehicals: this.decreaseVehicalNumber(this.state.destination1Vehicals, event),
+				});
+				break;
+			case 'Destination2':
+				if(this.state.selectedVehical2.length) {
+					await this.setState ({
+						destination2Vehicals: this.state.vehicals.map(vehical => { return {...vehical}}),
+						selectedVehical2: '',
+					});
+				}
+				this.setState ({
+					selectedVehical2: event,
+					destination2Vehicals: this.decreaseVehicalNumber(this.state.destination2Vehicals, event),
+				});
+				break;
+			case 'Destination3':
+				if(this.state.selectedVehical3.length) {
+					await this.setState ({
+						destination3Vehicals: this.state.vehicals.map(vehical => { return {...vehical}}),
+						selectedVehical3: '',
+					});
+				}
+				this.setState ({
+					selectedVehical3: event,
+					destination3Vehicals: this.decreaseVehicalNumber(this.state.destination3Vehicals, event),
+				});
+				break;
+			case 'Destination4':
+				if(this.state.selectedVehical4.length) {
+					await this.setState ({
+						destination4Vehicals: this.state.vehicals.map(vehical => { return {...vehical}}),
+						selectedVehical4: '',
+					});
+				}
+				this.setState ({
+					selectedVehical4: event,
+					destination4Vehicals: this.decreaseVehicalNumber(this.state.destination4Vehicals, event),
+				});
+				break;
+			default:
+		}
+	}
+
+	getVehicalsObject (destinationVehicals, selectedPlanet, selectedVehical) {
+		if(selectedVehical.length) {
+			return destinationVehicals.map(vehical => {
+				if(vehical.max_distance < this.getSelectedPlanetDistance(selectedPlanet) || (vehical.total_no === 0 && vehical.name !== selectedVehical)) {
+					return { value:vehical.name, label:`${vehical.name} (${vehical.total_no})`, itemClassName:"disabled" }
+				} else {
+					return { value:vehical.name, label:`${vehical.name} (${vehical.total_no})`}
+				}
+			});
+		} else {
+			const {selectedVehical1, selectedVehical2, selectedVehical3, selectedVehical4} = this.state;
+			return this.state.vehicals.map(vehicall => {
+				const vehical = {...vehicall};
+				if(selectedVehical1 === vehical.name) {
+					vehical.total_no -= 1;
+				}
+				if(selectedVehical2 === vehical.name) {
+					vehical.total_no -= 1;
+				}
+				if(selectedVehical3 === vehical.name) {
+					vehical.total_no -= 1;
+				}
+				if(selectedVehical4 === vehical.name) {
+					vehical.total_no -= 1;
+				}
+
+				if(vehical.max_distance < this.getSelectedPlanetDistance(selectedPlanet) || vehical.total_no === 0) {
+					return { value:vehical.name, label:`${vehical.name} (${vehical.total_no})`, itemClassName:"disabled" }
+				} else {
+					return { value:vehical.name, label:`${vehical.name} (${vehical.total_no})` }
+				}
+			});
+		}
 	}
 
 	getFilteredVehicals(destination) {
@@ -102,144 +287,39 @@ export default class Destination extends Component {
 		}
 	}
 
-	getCount(destination) {
-		let count = 0;
-		switch(destination) {
-			case 'Destination4':
-				count += this.getSpeed(this.state.selectedVehical4);
-			case 'Destination3':
-				count += this.getSpeed(this.state.selectedVehical3);
-			case 'Destination2':
-				count += this.getSpeed(this.state.selectedVehical2);
-			case 'Destination1':
-				count += this.getSpeed(this.state.selectedVehical1);
-			default:
-		}
-		return count;
-	}
-
 	planetSelect(event, destination) {
+		let vehicals = this.state.vehicals.map(vehical => { return {...vehical}});
 		switch(destination) {
 			case 'Destination1':
 				this.setState ({
 					selectedPlanet1: event.value,
-					destination1Vehicals: this.state.vehicals.map(vehical => { return {...vehical}}),
+					destination1Vehicals: vehicals,
 					selectedVehical1: '',
-					count: 0,
 				});
 				break;
 			case 'Destination2':
 				this.setState ({
 					selectedPlanet2: event.value,
-					destination2Vehicals: this.state.destination1Vehicals.map(vehical => { return {...vehical}}),
+					destination2Vehicals: vehicals,
 					selectedVehical2: '',
-					count: this.getCount('Destination1'),
 				});
-				debugger;
 				break;
 			case 'Destination3':
 				this.setState ({
 					selectedPlanet3: event.value,
-					destination3Vehicals: this.state.destination2Vehicals.map(vehical => { return {...vehical}}),
+					destination3Vehicals: vehicals,
 					selectedVehical3: '',
-					count: this.getCount('Destination2'),
 				});
 				break;
 			case 'Destination4':
 				this.setState ({
 					selectedPlanet4: event.value,
-					destination4Vehicals: this.state.destination3Vehicals.map(vehical => { return {...vehical}}),
+					destination4Vehicals: vehicals,
 					selectedVehical4: '',
-					count: this.getCount('Destination3'),
 				});
 				break;
 			default:
 				break;
-		}
-	}
-
-	increaseVehicalNumber(vehicals, vehicalName) {
-		 return vehicals.map(vehical => { if(vehical.name === vehicalName) { vehical.total_no +=1;} return vehical});
-	}
-
-	decreaseVehicalNumber(vehicals, vehicalName) {
-		return vehicals.map(vehicalObj => { if(vehicalObj.name === vehicalName) { vehicalObj.total_no -=1;} return vehicalObj});
-	}
-
-	getSpeed(selectedVehical) {
-		if(selectedVehical.length) {
-			return this.state.vehicals.filter(vehical => { if(vehical.name === selectedVehical) { return vehical}})[0].speed
-		}
-		return 0;
-	}
-
-	vehicalSelect (event, destination) {
-		let count = this.getSpeed(event);
-		switch (destination) {
-			case 'Destination1':
-				if(this.state.selectedVehical1.length) {
-					this.setState ({
-					  destination1Vehicals: this.increaseVehicalNumber(this.state.destination1Vehicals, this.state.selectedVehical1),
-						selectedVehical2: '',
-						selectedVehical3: '',
-						selectedVehical4: '',
-						selectedPlanet2: '',
-						selectedPlanet3: '',
-						selectedPlanet4: '',
-					});
-				}
-				this.setState ({
-					selectedVehical1: event,
-					destination1Vehicals: this.decreaseVehicalNumber(this.state.destination1Vehicals, event),
-					count: count,
-				});
-				break;
-			case 'Destination2':
-				count += this.getCount('Destination1');
-				if(this.state.selectedVehical2.length) {
-					this.setState ({
-					  destination2Vehicals: this.increaseVehicalNumber(this.state.destination2Vehicals, this.state.selectedVehical2),
-						selectedVehical3: '',
-						selectedVehical4: '',
-						selectedPlanet3: '',
-						selectedPlanet4: '',
-					});
-				}
-				this.setState ({
-					selectedVehical2: event,
-					destination2Vehicals: this.decreaseVehicalNumber(this.state.destination2Vehicals, event),
-					count: count,
-				});
-				break;
-			case 'Destination3':
-				count += this.getCount('Destination2');
-				if(this.state.selectedVehical3.length) {
-					this.setState ({
-					  destination3Vehicals: this.increaseVehicalNumber(this.state.destination3Vehicals, this.state.selectedVehical3),
-						selectedVehical4: '',
-						selectedPlanet4: '',
-					});
-				}
-				this.setState ({
-					selectedVehical3: event,
-					destination3Vehicals: this.decreaseVehicalNumber(this.state.destination3Vehicals, event),
-					count: count,
-				});
-				break;
-			case 'Destination4':
-				count += this.getCount('Destination3');
-				if(this.state.selectedVehical4.length) {
-					this.setState ({
-					  destination4Vehicals: this.increaseVehicalNumber(this.state.destination4Vehicals, this.state.selectedVehical4),
-					});
-				}
-				this.setState ({
-					selectedVehical4: event,
-					destination4Vehicals: this.decreaseVehicalNumber(this.state.destination4Vehicals, event),
-					count: count,
-				});
-				break;
-			default:
 		}
 	}
 
@@ -267,11 +347,11 @@ export default class Destination extends Component {
 							destination='Destination2'
 							planets={planets}
 							vehicalClass='vehicalRadioButton2'
-							vehicals={this.state.selectedPlanet2 && this.state.selectedVehical1 ? this.getFilteredVehicals('Destination2') : []}
+							vehicals={this.state.selectedPlanet2 ? this.getFilteredVehicals('Destination2') : []}
 							planetSelect={this.planetSelect}
 							selectedVehical={this.state.selectedVehical2}
 							vehicalSelect={this.vehicalSelect}
-							selectDisable={this.state.selectedVehical1.length ? false : true}
+							selectDisable={ false}
 						/>
 					</span>
 					<span>
@@ -279,11 +359,11 @@ export default class Destination extends Component {
 							destination='Destination3'
 							planets={planets}
 							vehicalClass='vehicalRadioButton3'
-							vehicals={this.state.selectedPlanet3 && this.state.selectedVehical2 ? this.getFilteredVehicals('Destination3') : []}
+							vehicals={this.state.selectedPlanet3 ? this.getFilteredVehicals('Destination3') : []}
 							planetSelect={this.planetSelect}
 							selectedVehical={this.state.selectedVehical3}
 							vehicalSelect={this.vehicalSelect}
-							selectDisable={this.state.selectedVehical2.length ? false: true}
+							selectDisable={ false}
 						/>
 					</span>
 					<span>
@@ -291,11 +371,11 @@ export default class Destination extends Component {
 							destination='Destination4'
 							planets={planets}
 							vehicalClass='vehicalRadioButton4'
-							vehicals={this.state.selectedPlanet4 && this.state.selectedVehical3 ? this.getFilteredVehicals('Destination4') : []}
+							vehicals={this.state.selectedPlanet4 ? this.getFilteredVehicals('Destination4') : []}
 							planetSelect={this.planetSelect}
 							selectedVehical={this.state.selectedVehical4}
 							vehicalSelect={this.vehicalSelect}
-							selectDisable={this.state.selectedVehical3.length ? false : true}
+							selectDisable={ false }
 						/>
 					</span>
 				  <span><h2>Time Taken: {this.state.count} </h2></span>
